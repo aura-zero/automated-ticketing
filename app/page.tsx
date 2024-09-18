3ee9b9151db72fc4b5c9c9163c9ec48454c6a114
abhishek <akyv2.5@gmil.com>
2024-09-18 08:38:18 +0530

    nextui nextjs project setup~

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <h1 className={title()}></h1>
+        <h2 className={subtitle({ class: "mt-4" })}></h2>
+      </div>
+      <div className="flex gap-3"></div>
+
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            <Code color="primary">app/page.tsx</Code>
+          </span>
+        </Snippet>
+      </div>
+    </section>
+  );
+}
